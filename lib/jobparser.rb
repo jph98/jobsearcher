ee913fe136bd179a9aa4be0b35652d5deea14b29
Jon <jonathan.holloway@gmail.com>
2014-04-26 22:35:38 +0100

    Initial working version

--- lib/jobparser.rb
+++ lib/jobparser.rb
@@ -5,34 +5,72 @@ require 'nokogiri'
 require 'open-uri'
 require 'mechanize'
 
-class JobParser
+class StarScreamJobParser
 
-	attr_accessor :match
-
-	def initialize()    
-		@baseurl = "http://www.cwjobs.co.uk"
+   	def get_detail(page, name)
+   		page.search("//td[contains(., '" + name + "')]/following-sibling::td").text().strip()
    	end
 
-	# Parse football fixtures/results
+   	def scrape_page_details(page)
+
+   		job_detail = page.search(".jobText").text
+   		puts get_detail(page, "Advertiser")
+   		puts get_detail(page, "Contact Name")
+   		puts get_detail(page, "Telephone")
+   		puts get_detail(page, "Reference")
+   		puts get_detail(page, "Salary")
+   		puts get_detail(page, "Location")
+   		puts get_detail(page, "Job Type")
+   		puts get_detail(page, "Date Posted")
+   		puts get_detail(page, "Last Updated Date")
+
+   	end
+
+   	def parse_all_jobs_on_page(job_page)
+
+		job_page.links().each do |l|
+			if l.attributes["itemprop"] != nil
+				puts "Found job: " + l.text
+				scrape_page_details(l.click())
+				puts "\n"
+			end		
+		end
+   	end
+
+   	def handle_jobs_page(page_number, job_page)
+
+   		puts "Handling job page no. #{page_number}"
+
+		parse_all_jobs_on_page(job_page)
+
+   		next_link = job_page.link_with(:id => "NextPage")
+
+		if (next_link != nil)
+			next_page = next_link.click
+			page_number = page_number + 1
+			handle_jobs_page(page_number, next_page)			
+		end
+   	end
+
    	def parse()
-   		a = Mechanize.new { |agent|
+
+   		starscream = Mechanize.new do |agent|
   			agent.user_agent_alias = 'Mac Safari'
-		}
+		end
 
-		a.get('http://google.com/') do |page|
+		starscream.get('http://www.theitjobboard.co.uk/') do |home_page|
 
-		  search_result = page.form_with(:name => 'f') do |search|
-		    search.q = 'Hello world'
-		  end.submit
+			first_job_page = home_page.form_with(:id => "SearchForm") do |form|
+				form["SearchTerms"] = "Java"
+				form["LocationSearchTerms"] = "Bristol"
+			end.submit
 
-		  search_result.links.each do |link|
-		    puts link.text
-		  end
-		  
+			page_number = 1
+			handle_jobs_page(page_number, first_job_page)
 		end
 	end
 
 end
 
-parser = JobParser.new()
+parser = StarScreamJobParser.new()
 parser.parse()
